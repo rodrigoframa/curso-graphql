edd12dcda90326734c981b0857557ad824d850dd
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2019-06-23 12:26:59 -0300

    feat(mutation): alterar  usuario

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -27,6 +27,22 @@ module.exports = {
 
         const excluidos = usuarios.splice(i, 1)
         return excluidos ? excluidos[0] : null
+    },
+    alterarUsuario(_, args) {
+        const i = usuarios
+            .findIndex(u => u.id === args.id)
+        
+        if (i < 0) return null
+
+        const usuario = {
+            ...usuarios[i],
+            ...args
+        }
+
+        usuarios.splice(i, 1, usuario)
+
+        return usuario
+        
     }
 
 }
