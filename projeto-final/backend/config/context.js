ab867e147d1ae7ca178fc31ccb58c664ad2e52fe
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2019-06-24 01:50:00 -0300

    chore: adicao de token na requisicao cliente

--- projeto-final/backend/config/context.js
+++ projeto-final/backend/config/context.js
@@ -1,9 +1,45 @@
+const jwt = require('jwt-simple')
+
 module.exports = async ({ req }) => {
     // Em desenvolvimento
 
     await require('./simularUsuarioLogado')(req)
-    
+
     const auth = req.headers.authorization
-    console.log(auth)
+    const token = auth && auth.substring(7)
+
+    let usuario = null
+    let admin = false
+    
+    if(token) {
+        try {
+            let conteudoToken = jwt.decode(token,
+                process.env.APP_AUTH_SECRET)
+            
+            if(new Date(conteudoToken.exp * 1000 > new Date())) {
+                usuario = conteudoToken
+            }
+        } catch (e) {
+            // token invalido
+        }
+    }
+
+    if(usuario && usuario.perfis) {
+        admin = usuario.perfis.includes('admin')
+
+    }
+
+    const err = new Error('Acesso negado!')
+
+    return {
+        usuario, 
+        admin , 
+        validarUsuario() {
+            if(!usuario) throw err
+        },
+        validarAdmin() {
+            if(!admin) throw err
+        }
+    }
 
 }
