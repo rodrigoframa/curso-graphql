dbd097fb383389cea743f4990e59782983764ee5
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2019-07-01 00:33:08 -0300

    chore(usuarios): listar usuarios

--- projeto-final/frontend/src/componentes/usuario/Lista.vue
+++ projeto-final/frontend/src/componentes/usuario/Lista.vue
@@ -31,6 +31,7 @@
 
 <script>
 import Erros from '../comum/Erros'
+import gql from 'graphql-tag'
 
 export default {
     components: { Erros },
@@ -48,7 +49,23 @@ export default {
     },
     methods: {
         obterUsuarios() {
-            console.log(this.$api)
+            this.$api.query({
+                query: gql`
+                    query {
+                        usuarios {
+                            id nome email perfis { nome rotulo }
+                        }
+                    }
+                
+                `,
+                fetchPolicy: 'network-only'
+            }).then(resultado => {
+                this.usuarios = resultado.data.usuarios
+                this.erros = null
+            }).catch(e => {
+                this.usuarios = []
+                this.erros = e
+            })
         }
     }
 }
