909497e302ac80a7649cb6ffee87aaf75ef11116
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2019-06-30 21:54:06 -0300

    chore: validacao na busca de usuarios e perfis somente usuarios logados e tipo admin

--- projeto-final/backend/resolvers/Query/usuario.js
+++ projeto-final/backend/resolvers/Query/usuario.js
@@ -20,10 +20,13 @@ module.exports = {
 
         return getUsuarioLogado(usuario)
     },
-    usuarios() {
+    usuarios(parent, args, ctx) {
+        ctx && ctx.validarAdmin()
         return db('usuarios')
     },
-    usuario(_, { filtro }) {
+    usuario(_, { filtro }, ctx) {
+        ctx && ctx.validarUsuarioFiltro(filtro)
+        
         if(!filtro) return null
         const { id, email } = filtro
         if(id) {
