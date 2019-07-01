febcc2f4798f187044ecd7930691366b14e1275e
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2019-06-30 21:50:45 -0300

    chore: validacao para alterar perfil de usuario somente se for admin

--- projeto-final/backend/config/simularUsuarioLogado.js
+++ projeto-final/backend/config/simularUsuarioLogado.js
@@ -23,7 +23,7 @@ const getUsuario = async nomePerfil => {
 }
 
 module.exports = async req => {
-    const usuario = await getUsuario('comum')
+    const usuario = await getUsuario('admin')
 
     if(usuario) {
         const { token } = await getUsuarioLogado(usuario)
